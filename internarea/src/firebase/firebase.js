174c6d31a0bfb25189e0ff7aa486cefc7597bb77
Pooja <poojarypooja987@gmail.com>
2026-06-19 15:47:26 +0530

    Fix Firebase config

--- internarea/src/firebase/firebase.js
+++ internarea/src/firebase/firebase.js
@@ -1,16 +1,17 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDbYTYs_oSK2rvtzHxBCPFrqdXM-l7MokQ",
-  authDomain: "internshala-9e300.firebaseapp.com",
-  projectId: "internshala-9e300",
-  storageBucket: "internshala-9e300.firebasestorage.app",
-  messagingSenderId: "304425115290",
-  appId: "1:304425115290:web:1b0653f1acd4192b848923",
+  apiKey: "AIzaSyC7VosA1EFkXsbKVqstligFQwSYok1gOKc",
+  authDomain: "internshala-clone-36d62.firebaseapp.com",
+  projectId: "internshala-clone-36d62",
+  storageBucket: "internshala-clone-36d62.firebasestorage.app",
+  messagingSenderId: "632606293380",
+  appId: "1:632606293380:web:f0b3834231447708d631d1"
 };
 
 // Initialize Firebase
